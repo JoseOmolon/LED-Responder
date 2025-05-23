8e18cb626cbcf5a179cf97b6e46ab74d089e9df1
Jose <159615295+JoseOmolon@users.noreply.github.com>
2025-05-23 13:47:31 +0200

    Update LED_Responder.ino

--- LED_Responder.ino
+++ LED_Responder.ino
@@ -1,12 +1,12 @@
-int Redled=8;
-int Yellowled=7;
-int Greenled=6;
+int Redled=8;      // set Red LED as "output"
+int Yellowled=7;   // set Yellow LED as "output"
+int Greenled=6;    // set Green LED as "output"
 
-int Key1=5;
-int Key2=4;
-int Key3=3;
+int Key1=5;        // initialize pin for Red button
+int Key2=4;        // initialize pin for Yellow button
+int Key3=3;        // initialize pin for Green button
 
-int KeyRest=2;
+int KeyRest=2;     // initialize pin for Reset button
 
 int Red;
 int Yellow;
